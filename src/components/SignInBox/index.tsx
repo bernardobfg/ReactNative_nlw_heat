e7c4f26e2012ea5badfaa249a65db9ade155eaa5
Bernardo Bevilaqua <bernardo.bfg@hotmail.com>
2021-10-22 11:49:00 -0300

    auth logic done

--- src/components/SignInBox/index.tsx
+++ src/components/SignInBox/index.tsx
@@ -6,8 +6,10 @@ import {
 import { styles } from './styles';
 import { Button } from '../Button';
 import { COLORS } from '../../theme';
+import { useAuth } from '../../hooks/auth';
 
-export function SignInBox(){
+export function SignInBox() {
+  const {signIn, isSigningIn} = useAuth()
   return (
     <View style={styles.container}>
       <Button
@@ -15,7 +17,8 @@ export function SignInBox(){
         color={COLORS.BLACK_PRIMARY}
         backgroundColor={COLORS.YELLOW}
         icon="github"
-        isLoading={true}
+        onPress={signIn}
+        isLoading={isSigningIn}
       />
     </View>
   );
