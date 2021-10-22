e3b13c19ed0481ea8bd710d582eb68d6bbfedda9
Bernardo Bevilaqua <bernardo.bfg@hotmail.com>
2021-10-22 10:29:25 -0300

    finish styling

---
+++ src/components/SignInBox/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {
+  View
+} from 'react-native';
+import { styles } from './styles';
+import { Button } from '../Button';
+import { COLORS } from '../../theme';
+
+export function SignInBox(){
+  return (
+    <View style={styles.container}>
+      <Button
+        title="ENTRAR COM GITHUB"
+        color={COLORS.BLACK_PRIMARY}
+        backgroundColor={COLORS.YELLOW}
+        icon="github"
+        isLoading={true}
+      />
+    </View>
+  );
+}
